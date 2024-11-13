f61a4d5ee0f11825369568f49192ea208114f699
Elijah Gutman <gutmane7@gmail.com>
2024-11-12 20:24:22 -0500

    Tests resvoled, all data functioning in database

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -3,4 +3,19 @@ class SystemsController < ApplicationController
     @systems = System.all
     render :index
   end
+
+  def create
+    @system = System.create(
+      country_name: params[:country_name],
+      economic_system: params[:economic_system],
+      economic_index: params[:economic_index],
+      governmental_system: params[:governmental_system],
+      governmental_index: params[:governmental_index],
+      cultural_system: params[:cultural_system],
+      cultural_index: params[:cultural_index],
+      image_url: params[:image_url],
+      wiki_page: params[:wiki_page],
+    )
+    render :show
+  end
 end
--- test/controllers/systems_controller_test.rb
+++ test/controllers/systems_controller_test.rb
@@ -8,4 +8,10 @@ class SystemsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal System.count, data.length
   end
+  test "create" do
+    assert_difference "System.count", 1 do
+      post "/systems.json", params: { country_name: "Peru", economic_system: "Capitalism", economic_index: 1, governmental_system: "Democracy", governmental_index: 1, cultural_system: "Liberalism", cultural_index: 1, image_url: "test.jpg", wiki_page: "testwikipage.com" }
+      assert_response 200
+    end
+  end
 end
